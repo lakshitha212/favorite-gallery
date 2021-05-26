33800d8c515f4771f626ae454f5d5b999f7e9e4e
M.T.C Lakshitha <chameera@encyteus.com>
2021-05-26 23:30:26 +0530

    redis for cache

--- back-end/server/app.js
+++ back-end/server/app.js
@@ -4,11 +4,32 @@ import dotenv from 'dotenv'
 import path from 'path'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser';
-import logger from 'morgan'
+
 import indexRouter from './routes/index'
 import cors from 'cors';
+
+const redis = require("redis");
+
+const redisPort = process.env.REDIS_PORT || 6379
+const client = redis.createClient(redisPort);
+
+client.on("error", (err) => {
+    console.log(err);
+})
+
+/**
+ * Enable Loggin
+ * Save log files in ../log
+ */
+import morgan  from 'morgan'
+const rfs = require("rotating-file-stream");
+
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", 
+  path: path.join(__dirname, "../log"),
+});
+
 import {
-  postTest,
   postEntries,
   postEntry,
   rearrangeEntries
@@ -23,9 +44,9 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, '../public')))
-
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use('/', indexRouter)
-// app.get('/create-test', makeCallback(postTest))
+
 app.post('/get-entries', makeCallback(postEntries))
 app.post('/update-entry', makeCallback(postEntry))
 app.post('/sort-entries', makeCallback(rearrangeEntries))
