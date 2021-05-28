d4540d1ad7fb200faf4b2c755b339fe2ce06b057
Chameera Lakshitha <chameera.lakshitha212@gmail.com>
2021-05-28 09:57:16 +0530

    Update index.js

--- back-end/server/data-access/index.js
+++ back-end/server/data-access/index.js
@@ -13,9 +13,6 @@ const mongoPort = MONGO_PORT || 27017
 const mongoDb = MONGO_DB || "favorite"
 const mongoHost = MONGO_HOST || "mongo"
 
-// const url = process.env.USER_DB_URL || "mongodb+srv://dbuser:Mfe%40%2623LGC2@itspts-cluster-23t46.mongodb.net/test?retryWrites=true&w=majority"
-// const client = new MongoClient(url, { useUnifiedTopology: true,useNewUrlParser: true })
-
 const client = new MongoClient(`mongodb://${mongoHost}:${mongoPort}/${mongoDb}`, { useUnifiedTopology: true, useNewUrlParser: true })
 export async function makeDb () {
   if (!client.isConnected()) {
